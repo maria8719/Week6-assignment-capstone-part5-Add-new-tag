5bebab627c8ef0e1045e94671842d0769fef1ca6
maria <mabdallaoui6478@students.mc3.edu>
2022-02-21 20:39:25 -0500

    capstone part1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+const URL = "https://api.hatchways.io/assessment/students";
+
 function App() {
+
+  const [studentData, setStudentData] = useState([]); 
+  
+  const getStudents = (URL) => {
+     fetch(URL)
+      .then(response => response.json())
+      .then(data => setStudentData(data.students))
+      .catch(error => alert('Something went wrong!'));
+  }
+
+
+function averageGrades(grades){
+  let sum = 0;
+    for (let i=0; i < grades.length; i++) {
+        sum += parseInt(grades[i]);
+    }
+        return  sum / grades.length;
+
+}
+
+  useEffect(() => {
+    getStudents(URL);
+  }, []); 
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+    <ul>
+     {
+       studentData && 
+        studentData.map((student) => {
+          return (
+            <div> <img src = {student.pic} /> 
+               <h2>{student.firstName} {student.lastName}
+               </h2> 
+               <div>
+                Email: {student.email} 
+                </div>
+                <div>
+                Company: {student.company} 
+                </div>
+                <div>
+                Skill: {student.skill} 
+                </div>
+                <div>
+                Average: {averageGrades(student.grades)}%
+                </div>
+            </div>
+             
+           
+          )
+        })
+     }
+     </ul>
+    </main>
   );
 }
 
 export default App;
+
